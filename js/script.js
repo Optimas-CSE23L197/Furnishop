37481e6acfd7a33e9f234b173b11fadeb478abd4
Animesh Karan <karananimesh144@gmail.com>
2025-08-15 21:29:01 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('hambarger').addEventListener("click", () => {
+    document.querySelector('.nav_wrapper .nav_right ul').classList.toggle('show-menu')
+    console.log("hello");
+})
